295e44f3194dd335e62f08909c6da1e9fe3d6320
Petr Ilgner <petr@ilgner.cz>
2024-04-20 20:52:40 +0200

    System monitor - added temperature

--- sysmon.py
+++ sysmon.py
@@ -1,14 +1,28 @@
+import re
 import psutil
-import time
+import subprocess
+
+config_temp_sensor = None
 
 sys_stats = {
     'cpu': 0.0,
-    'memory': 0.0,
+    'memory': 0.0
 }
 
 
 def update():
+
+    if config_temp_sensor:
+        pattern = r'{}:\s*\+(.*?)\s*°C'.format(config_temp_sensor)
+        try:
+            output = subprocess.check_output(['sensors']).decode('utf-8')
+            match = re.search(pattern, output)
+            if match:
+                sys_stats['temp'] = match.group(1).strip()
+
+        except subprocess.CalledProcessError as e:
+            print(f"[SYSMON] Error: Failed to run sensors command: {e}")
+
     sys_stats['memory'] = psutil.virtual_memory().percent
     sys_stats['cpu'] = psutil.cpu_percent(interval=15)
-
 
